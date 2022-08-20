6a251ff5a18edac6b55bc29cfc9c018d9edea816
Aviv <aviv.dror@gmail.com>
2022-08-20 03:47:44 +0200

    get works

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+import { get } from "./api/get";
+
 function App() {
+  console.log("reload");
+  const [works, setWorks] = useState([]);
+
+  const worksInit = async () => {
+    const workPromise = await get("works?populate=media");
+    setWorks(workPromise);
+  };
+
+  useEffect(() => {
+    worksInit();
+    console.log("works", works);
+  }, []);
+
   return (
     <div className="App">
       <h1 className="test">aviv</h1>
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const ORIGIN = "https://ppp-page.herokuapp.com/api/";
